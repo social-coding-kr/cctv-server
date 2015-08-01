035b9b5a318f0fddd5f6b01d218dcd36cff1d978
joke111 <alta.second@gmail.com>
2015-08-01 22:10:34 +0900

    불필요한 로그 삭제

--- src/main/java/com/socialcoding/domain/services/CctvService.java
+++ src/main/java/com/socialcoding/domain/services/CctvService.java
@@ -2,19 +2,15 @@ package com.socialcoding.domain.services;
 
 import com.socialcoding.domain.models.Cctv;
 import com.socialcoding.domain.repositories.CctvRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class CctvService {
 	@Autowired
 	private CctvRepository cctvRepository;
 
 	public Cctv getCctvDetailById(long cctvId) {
-		Cctv cctv = cctvRepository.findOneWithComments(cctvId);
-		log.debug("test: {}", cctv);
-		return cctv;
+		return cctvRepository.findOneWithComments(cctvId);
 	}
 }
